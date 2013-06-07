eeac1f13974acaf00ad57cc721d03e76b872d7cf
wangkangluo1 <wangkangluo1@gmail.com>
2013-06-07 03:21:26 +0000

    Change mysql

--- mysqldump.sh
+++ mysqldump.sh
@@ -1,1 +1,10 @@
-/opt/lampp/bin/mysqldump drupal > /opt/lampp/htdocs/drupal/drupal.sql
+case $1 in
+	"backup")
+		/opt/lampp/bin/mysqldump drupal > /opt/lampp/htdocs/drupal/drupal.sql;;
+	"restore")
+		/opt/lampp/bin/mysql -e "use drupal; source /opt/lampp/htdocs/drupal/drupal.sql;";;
+        *)
+		echo "backup    backup the mysql to drupal.sql."
+                echo "restore   restore the drupal.sql to database drupal."
+                ;;
+esac
